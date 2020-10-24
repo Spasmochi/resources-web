5d01d711db62383e4d1897c06b8411f4ab34252b
Spasmodic <sammcmurtrie@outlook.com>
2020-10-24 13:01:15 +1100

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Web Resources</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  )
+}
