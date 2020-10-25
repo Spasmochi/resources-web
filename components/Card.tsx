e7904622c3a102021354e69d7c6f965b53e53c5e
Spasmodic <sammcmurtrie@outlook.com>
2020-10-25 17:51:02 +1100

    updated card styles

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,11 +10,17 @@ interface Props {
 
 export const Card: FC<Props> = ({ description, url, topics }: Props) => {
   return (
-    <div>
+    <div className="flex flex-col">
       <Link href={url}>
         <a className="col-span-1 rounded-lg">{description}</a>
       </Link>
-      {topics ? topics.map(topic => <SimpleBadge label={topic} />) : ""}
+      <div>
+        {topics
+          ? topics.map(topic => (
+              <SimpleBadge key={topic} label={topic} size={"lg"} />
+            ))
+          : ""}
+      </div>
     </div>
   )
 }
